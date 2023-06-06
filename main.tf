2687a6be0347edf561f22b2e71beeaee502f09a5
Borys Kovba <borys.kovba@gmail.com>
2023-06-06 12:23:54 +0300

    add terraform

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-3"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-05a8450aee7da05fb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
